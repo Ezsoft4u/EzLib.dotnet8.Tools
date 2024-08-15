2d5c9a0e734aaf4d40bcfbe92494061b09b0978f
MarkChu <markchu929@gmail.com>
2024-08-15 15:18:16 +0800

    update HinetSms

---
+++ Models/SmsVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzLib.Models
+{
+    public class SmsResult
+    {
+        /// <summary>
+        /// 發送結果
+        /// </summary>
+        public bool IsSuccess { get; set; }
+        /// <summary>
+        /// 錯誤訊息
+        /// </summary>
+        public string? Message { get; set; }
+    }
+}
---
+++ Services/HiNetSmsSender.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using EzLib.Services;
+
+namespace EzLib.Services
+{
+    public class HiNetSmsSender
+    {
+        private readonly Hiair2Net _hiair;
+        private readonly string _serverIp;
+        private readonly int _port;
+        private readonly string _username;
+        private readonly string _password;
+
+        public HiNetSmsSender(string serverIp, int port, string username, string password)
+        {
+            _hiair = new Hiair2Net();
+            _serverIp = serverIp;
+            _port = port;
+            _username = username;
+            _password = password;
+        }
+
+        public int Connect()
+        {
+            return _hiair.StartCon(_serverIp, _port, _username, _password);
+        }
+
+        public string GetMessage()
+        {
+            return _hiair.Get_Message();
+        }
+
+        public int SendMessage(string phoneNumber, string message)
+        {
+            return _hiair.SendMsg(phoneNumber, message);
+        }
+
+        public void Disconnect()
+        {
+            _hiair.EndCon();
+        }
+    }
+}
